7b9541c1ea7bc4185b7f16dab844ad90ea8fc691
kost9klinov <kost9klinov>
2018-09-08 17:44:12 +0300

    Initial Commit

---
+++ GIFSearcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GIFSearcher
+//
+//  Created by Константин Клинов on 9/8/18.
+//  Copyright © 2018 Константин Клинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
